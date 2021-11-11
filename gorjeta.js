7f217b6fa3053416d07b3582777913fca44d3d78
Koohler <90351653+Koohler@users.noreply.github.com>
2021-11-11 13:50:34 -0300

    Create gorjeta.js

---
+++ gorjeta.js
@@ -0,0 +1,36 @@
+.container {
+    height: 500px;
+    width: 360px;
+    margin: 70px auto;
+    background: var(--cor3);
+    box-shadow: -5px 0px 10px var(--cor3);
+    border-radius: 10px;
+}
+.title {
+    background: var(--cor2);
+    color: white;
+    padding: 20px;
+    text-transform: uppercase;
+    font-size: 24px;
+    font-weight: normal;
+    border-radius: 10px 10px 0 0;
+    text-align: center;
+    letter-spacing: .7px;
+}
+.tipsForm {
+    padding-left: 20px;
+    padding-right: 20px;
+}
+.formInput {
+    background: white;
+    max-width: 40px;
+    padding: 5px 5px 8px 8px;
+}
+.formInput:focus {
+    border: 3px solid var(--cor1); 
+}
+.formSelect {
+    width: 180px;
+    padding: 5px;
+    font-size: 12px;
+}
